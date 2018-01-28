b6317a47062c9b3ce8d1794ab5ad77f03decbd60
Martin Mirenić <32452116+marx5@users.noreply.github.com>
2018-01-28 18:09:16 +0100

    Delete dbconn.php

--- dbconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	# Stop Hacking attempt
-	if(!defined('__APP__')) {
-		die("Hacking attempt");
-	}
-	
-	# Connect to MySQL database
-	$MySQL = mysqli_connect("localhost","id4356488_root","roottoor","id4356488_webprog") or die('Error connecting to MySQL server.');
